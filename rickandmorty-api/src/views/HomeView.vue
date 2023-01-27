305027e9279b70e5584df943c83ba4a552d46336
wkaska <nestenic000@gmail.com>
2023-01-27 23:07:06 +0700

    vue
    vue

---
+++ rickandmorty-api/src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Rick and Morty API</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+import { response } from 'express';
+
+export default {
+  name: 'HomeView',
+  components: {
+  },
+  data(){
+    return {
+
+    }
+  },
+  mounted(){
+    console.log('called')
+    this.getTodos();
+  },
+  methods: {
+    getTodos(){
+      console.log('received')
+      axios
+      .get('https://rickandmortyapi.com/api/character')
+      .then(response => {
+        console.log(response)
+      })
+      // (response => response.json())
+      .catch(error => {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
